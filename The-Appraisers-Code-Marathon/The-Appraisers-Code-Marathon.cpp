2a7187ef6ab3c8a43ebff73ac9e04b5a0a9501d3
afyusrefova <afyusrefova18@codingburgas.bg>
2021-06-26 18:09:02 +0300

    Initial commit

---
+++ The-Appraisers-Code-Marathon/The-Appraisers-Code-Marathon.cpp
@@ -0,0 +1,13 @@
+// The-Appraisers-Code-Marathon.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+
+
+}
