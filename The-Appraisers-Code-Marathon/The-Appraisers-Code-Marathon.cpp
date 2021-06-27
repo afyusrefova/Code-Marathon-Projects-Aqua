f74c329ca1ef2c07b97e64b246b0402ea689c857
kvbolashikova18 <59692632+kvbolashikova18@users.noreply.github.com>
2021-06-27 13:36:07 +0300

    Add needed libraries and define colors

--- The-Appraisers-Code-Marathon/The-Appraisers-Code-Marathon.cpp
+++ The-Appraisers-Code-Marathon/The-Appraisers-Code-Marathon.cpp
@@ -1,13 +1,33 @@
-// The-Appraisers-Code-Marathon.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
+
+#define RESET   "\033[0m"
+
+#define RED     "\033[1;91m" 
+#define YELLOW  "\033[1;93m"  
+#define GREEN   "\033[1;92m"
+#define BLUE    "\033[0;36m"
+#define CYAN    "\033[1;96m"
+#define PURPLE  "\033[1;95m"
+#define GRAY    "\033[1;90m" 
+#define MINT    "\033[1;96m"
+
+#define WHITE   "\033[4;37m"
+#define BWhite  "\033[1;37m"  
+#define UWhite  "\033[4;37m"  
+
+#define BBlack  "\033[1;30m"    
+#define BRed    "\033[1;31m"         
+#define BGreen  "\033[1;32m"       
+#define BYellow "\033[1;33m"      
+#define BBlue   "\033[1;34m"       
+#define BPurple "\033[1;35m"      
+#define BCyan   "\033[1;36m"       
+#define BWhite  "\033[1;37m"      
 
 int main()
 {
-    
-
-
 
 }
